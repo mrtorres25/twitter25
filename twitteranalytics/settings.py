a2c7953d770f39352540b210b8ce3409d2ff69d6
Samuel Hindley Lopez <samuelhindley@gmail.com>
2016-11-28 19:21:39 +0100

    users app initialized and configured

--- twitteranalytics/settings.py
+++ twitteranalytics/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'twitteranalytics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,4 +99,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
