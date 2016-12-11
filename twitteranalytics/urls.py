4908ac77aec52f36c87a91a9d4aaa31a1dabafa4
Samuel Hindley Lopez <samuelhindley@gmail.com>
2016-12-11 17:43:54 +0100

    urls fixed

--- twitteranalytics/urls.py
+++ twitteranalytics/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url(r'^search/', include('search.urls')),
     url(r'^login/', 'users.views.login', name = 'login'),
     url(r'^logout/', 'users.views.logout', name = 'logout'),
-    url(r'^searchurl/', include('searchByUrl.urls'), name = 'searchurl'),
+    url(r'^searchurl/', include('searchByUrl.urls')),
+    url(r'^location/', include('location.urls')),
     url(r'^$', 'users.views.login', name = 'home'),
-    url(r'^location/', include('location.urls'))
 ]
