901ea684aff541f841d6f6bdc18dec26c1feac46
Unknown <jmtorres252@hotmail.com>
2016-12-20 10:34:02 +0100

    strike1

--- executeTest.sh
+++ executeTest.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 python35 search/test/TestSearchRenfe.py
-python35 search/test/TestSearchNothing.py
-python35 search/test/TestSearchAfrikaans.py
-python35 search/test/TestSearchEnglish.py
