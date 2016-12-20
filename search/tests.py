616c6c3487d82b1a5afdc90e4606b5bcb90a2af8
Unknown <jmtorres252@hotmail.com>
2016-12-20 14:09:51 +0100

    REMOVE A TEST IN SEARCH

--- search/tests.py
+++ search/tests.py
@@ -69,64 +69,64 @@ class SearchRenfeCualquiera(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 
-class TestSearchEnglish(unittest.TestCase):
-    def setUp(self):
-        # binary = FirefoxBinary('C:/Program Files (x86)/Mozilla Firefox/firefox.exe')  # ESTO HAY QUE PONERLO PARA AUTOMATIZAR LAS PRUEBAS EN TU CASA
-        binary = FirefoxBinary(r'/opt/firefox/firefox')
-        self.driver = webdriver.Firefox(firefox_binary=binary)
-        self.driver.implicitly_wait(5)
-        self.base_url = "http://127.0.0.1:8080/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
-
-    def test_search_english(self):
-        driver = self.driver
-        driver.get(self.base_url)
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("root")
-        driver.find_element_by_name("username").clear()
-        driver.find_element_by_name("username").send_keys("root")
-        driver.find_element_by_xpath("//button[@type='submit']").click()
-        driver.find_element_by_id("toSearch").send_keys(Keys.ENTER)
-        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.alert.alert-info"))
-        # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
-        driver.find_element_by_id("searchbox").clear()
-        driver.find_element_by_id("searchbox").send_keys("Renfe")
-        # Select(driver.find_element_by_name("languageCode")).select_by_visible_text("English : English")
-        Select(driver.find_element_by_name("languageCode")).select_by_value("en")
-        driver.find_element_by_css_selector("button.btn").click()
-        self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.alert.alert-success"))
-        driver.find_element_by_link_text("Salir").click()
-
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
+# class TestSearchEnglish(unittest.TestCase):
+#     def setUp(self):
+#         # binary = FirefoxBinary('C:/Program Files (x86)/Mozilla Firefox/firefox.exe')  # ESTO HAY QUE PONERLO PARA AUTOMATIZAR LAS PRUEBAS EN TU CASA
+#         binary = FirefoxBinary(r'/opt/firefox/firefox')
+#         self.driver = webdriver.Firefox(firefox_binary=binary)
+#         self.driver.implicitly_wait(5)
+#         self.base_url = "http://127.0.0.1:8080/"
+#         self.verificationErrors = []
+#         self.accept_next_alert = True
+#
+#     def test_search_english(self):
+#         driver = self.driver
+#         driver.get(self.base_url)
+#         driver.find_element_by_name("password").clear()
+#         driver.find_element_by_name("password").send_keys("root")
+#         driver.find_element_by_name("username").clear()
+#         driver.find_element_by_name("username").send_keys("root")
+#         driver.find_element_by_xpath("//button[@type='submit']").click()
+#         driver.find_element_by_id("toSearch").send_keys(Keys.ENTER)
+#         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.alert.alert-info"))
+#         # ERROR: Caught exception [ERROR: Unsupported command [selectWindow | null | ]]
+#         driver.find_element_by_id("searchbox").clear()
+#         driver.find_element_by_id("searchbox").send_keys("Renfe")
+#         # Select(driver.find_element_by_name("languageCode")).select_by_visible_text("English : English")
+#         Select(driver.find_element_by_name("languageCode")).select_by_value("en")
+#         driver.find_element_by_css_selector("button.btn").click()
+#         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.alert.alert-success"))
+#         driver.find_element_by_link_text("Salir").click()
+#
+#     def is_element_present(self, how, what):
+#         try:
+#             self.driver.find_element(by=how, value=what)
+#         except NoSuchElementException as e:
+#             return False
+#         return True
+#
+#     def is_alert_present(self):
+#         try:
+#             self.driver.switch_to_alert()
+#         except NoAlertPresentException as e:
+#             return False
+#         return True
+#
+#     def close_alert_and_get_its_text(self):
+#         try:
+#             alert = self.driver.switch_to_alert()
+#             alert_text = alert.text
+#             if self.accept_next_alert:
+#                 alert.accept()
+#             else:
+#                 alert.dismiss()
+#             return alert_text
+#         finally:
+#             self.accept_next_alert = True
+#
+#     def tearDown(self):
+#         self.driver.quit()
+#         self.assertEqual([], self.verificationErrors)
 
 
 class SearchNothing(unittest.TestCase):
@@ -246,7 +246,5 @@ class TestSearchAfrikaans(unittest.TestCase):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
 
-
 if __name__ == "__main__":
     unittest.main()
-
