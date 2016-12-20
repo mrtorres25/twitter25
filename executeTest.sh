7ddaa4fc18108d143e9dbba7fe06990178cf90ae
Unknown <jmtorres252@hotmail.com>
2016-12-20 10:55:42 +0100

    Test TOMA1

--- executeTest.sh
+++ executeTest.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 python35 search/test/TestSearchRenfe.py
+python35 search/test/TestSearchEnglish.py
+python35 search/test/TestSearchAfrikaans.py
+python35 search/test/TestSearchNothing.py
