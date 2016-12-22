44b20ebea3e161100f815a4a9c373d48c3adbeb7
Samuel Hindley Lopez <samuelhindley@gmail.com>
2016-12-22 16:08:50 +0100

    users tests modified

--- users/tests.py
+++ users/tests.py
@@ -132,7 +132,8 @@ class Logout(unittest.TestCase):
         driver.find_element_by_xpath("//button[@type='submit']").click()
         self.assertTrue(self.is_element_present(By.CSS_SELECTOR, "div.alert.alert-success"))
         driver.find_element_by_link_text("Salir").click()
-        self.assertEqual("Login", driver.title)
+        time.sleep(10)
+        self.assertEqual("Home", driver.title)
     
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
