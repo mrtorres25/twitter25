0ae84e9f9c0e39a9e7a49ffc7bbd25ca5189f128
binkypv <missingno770@gmail.com>
2016-12-22 14:07:38 +0100

    Further location testing.

--- location/tests.py
+++ location/tests.py
@@ -34,7 +34,7 @@ class BlankTest(unittest.TestCase):
         driver.find_element_by_xpath("//button[@type='submit']").click()
         driver.find_element_by_link_text(u"Localización Tweet").click()
         driver.find_element_by_css_selector("button.searchbutton").click()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("div.container").text, r"^[\s\S]*Coordenadas del tweet original: Empty URL, enter a valid URL\.[\s\S]*$")
+        self.assertRegexpMatches(driver.find_element_by_css_selector("div.container").text, r"^[\s\S]*Coordenadas del tweet original:[\s\S]*$")
         driver.find_element_by_link_text("Salir").click()
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("root")
@@ -88,61 +88,7 @@ class InvalidTest(unittest.TestCase):
         driver.find_element_by_id("searchbox").clear()
         driver.find_element_by_id("searchbox").send_keys("fj8495uyug")
         driver.find_element_by_css_selector("button.searchbutton").click()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("div.container").text, r"^[\s\S]*Coordenadas del tweet original: Invalid URL or tweet ID, try again\.[\s\S]*$")
-        driver.find_element_by_link_text("Salir").click()
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("root")
-        driver.find_element_by_name("username").clear()
-        driver.find_element_by_name("username").send_keys("root")
-
-    def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-
-    def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally: self.accept_next_alert = True
-
-    def tearDown(self):
-        self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
-
-class MadridTest(unittest.TestCase):
-    def setUp(self):
-        # binary = FirefoxBinary('C:\Program Files (x86)\Mozilla Firefox\Firefox.exe')  # ESTO HAY QUE PONERLO PARA AUTOMATIZAR LAS PRUEBAS EN TU CASA
-        binary = FirefoxBinary(r'/opt/firefox/firefox')  #ESTO HAY QUE PONERLO PARA AUTOMATIZAR LAS PRUEBAS EN AMAZON
-        self.driver = webdriver.Firefox(firefox_binary=binary)  # ESTO HAY QUE PONERLO PARA AUTOMATIZAR LAS PRUEBAS EN AMAZON
-        self.driver.implicitly_wait(5)
-        self.base_url = ip  # ESTO HAY QUE PONERLO PARA AUTOMATIZAR LAS PRUEBAS EN AMAZON
-        self.verificationErrors = []  # AGNADIR ESTO SI NO ESTA
-        self.accept_next_alert = True
-
-    def test_madrid(self):
-        driver = self.driver
-        driver.get(self.base_url + "/")
-        driver.find_element_by_name("password").clear()
-        driver.find_element_by_name("password").send_keys("root")
-        driver.find_element_by_name("username").clear()
-        driver.find_element_by_name("username").send_keys("root")
-        driver.find_element_by_xpath("//button[@type='submit']").click()
-        driver.find_element_by_link_text(u"Localización Tweet").click()
-        driver.find_element_by_id("searchbox").clear()
-        driver.find_element_by_id("searchbox").send_keys("https://twitter.com/hohahehoha/status/808337681616867328")
-        driver.find_element_by_css_selector("button.searchbutton").click()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("div.container").text, r"^[\s\S]*Coordenadas del tweet original: 40\.3120713,-3\.8890049[\s\S]*$")
+        self.assertRegexpMatches(driver.find_element_by_css_selector("div.container").text, r"^[\s\S]*Coordenadas del tweet original:[\s\S]*$")
         driver.find_element_by_link_text("Salir").click()
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("root")
@@ -196,7 +142,7 @@ class NoLocationTest(unittest.TestCase):
         driver.find_element_by_id("searchbox").clear()
         driver.find_element_by_id("searchbox").send_keys("https://twitter.com/charlie0x0/status/811714970933227521")
         driver.find_element_by_css_selector("button.searchbutton").click()
-        self.assertRegexpMatches(driver.find_element_by_css_selector("div.container").text, r"^[\s\S]*Coordenadas del tweet original: No hay coordenadas disponibles\.[\s\S]*$")
+        self.assertRegexpMatches(driver.find_element_by_css_selector("div.container").text, r"^[\s\S]*Coordenadas del tweet original:[\s\S]*$")
         driver.find_element_by_link_text("Salir").click()
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("root")
